312841a1f1e02b125d6bc8b3770862fc5ea1141d
JuDePom <jimmy.etienne@aliceadsl.fr>
2015-12-02 18:27:44 +0100

    Commit so good

--- src/java/model/BluetoothManager.java
+++ src/java/model/BluetoothManager.java
@@ -10,8 +10,8 @@ import java.util.List;
 import controller.XBoxCtrlListener.Movement;
 
 public class BluetoothManager {
-	
-	
+
+
 	private Socket socket;
 	private BufferedReader bufferedReader;
 	int PORT = 5000;
@@ -23,58 +23,58 @@ public class BluetoothManager {
 		colours = new ArrayList<>();
 		gyro = new ArrayList<>();
 		dist = new ArrayList<>();
-	
-	
+
+
 	}
 
-	
+
 	public boolean connect(String ip){
 		try {
 			socket = new Socket(ip, PORT);
 			Thread t = new Thread() {
-				
+
 				@Override
 				public void run() {
 					listen();
 				}
 			};
 			t.start();
-			
+
 		} catch (Exception e){
 			return false;
 		}
 		return true;
-	
+
 	}
-	
+
 
 	public void listen(){
 		String fromclient;
 
-		
+
 		try {
 			bufferedReader = new BufferedReader(new InputStreamReader (socket.getInputStream()));
 
 			while( ! socket.isClosed()) {
-					fromclient = bufferedReader.readLine();
-					System.out.println(fromclient);
-					if(fromclient.startsWith("colors: ")){
-						setColors(fromclient.split("colors: ")[1]);
-					}
-					else if(fromclient.startsWith("gyro: ")){
-						setGyro(fromclient.split("gyro: ")[1]);
-					}
-					else if(fromclient.startsWith("dist: ")){
-						//setDist(fromclient.split("dist: ")[1]);
-					}
+				fromclient = bufferedReader.readLine();
+				System.out.println(fromclient);
+				if(fromclient.startsWith("colors: ")){
+					setColors(fromclient.split("colors: ")[1]);
+				}
+				else if(fromclient.startsWith("gyro: ")){
+					setGyro(fromclient.split("gyro: ")[1]);
+				}
+				else if(fromclient.startsWith("dist: ")){
+					//setDist(fromclient.split("dist: ")[1]);
+				}
 			}
 		} catch (IOException e) {
-			
+
 			//e.printStackTrace();
 			System.out.println(e.getMessage());
 		}
 	}
-	
+
 	private void setColors(String colors){
 		String[] tmp = colors.split(" ");
 		colours.clear();
@@ -82,8 +82,8 @@ public class BluetoothManager {
 			colours.add(Double.parseDouble(col));
 		}
 	}
-	
-	
+
+
 	private void setGyro(String s){
 		String[] g = s.split(" ");
 		gyro.clear();
@@ -91,7 +91,7 @@ public class BluetoothManager {
 			gyro.add(Double.parseDouble(val));
 		}
 	}
-	
+
 	private void setDist(String s){
 		String[] g = s.split(" ");
 		dist.clear();
@@ -99,37 +99,40 @@ public class BluetoothManager {
 			dist.add(Double.parseDouble(val));
 		}
 	}
-	
-	
+
+
 	public void sendMovement(Movement m) {
 		if (socket != null && socket.isConnected() && !socket.isClosed()){
 			try{
 				double radian = Math.toRadians(m.direction);
 				String toSend = "move:"
-								+Math.cos(radian)
-								+","
-								+Math.sin(radian)
-								+"\n";
+						+(Math.cos(radian) * m.magnitude)
+						+","
+						+(Math.sin(radian) * m.magnitude)
+						+"\n";
 				
+				System.out.println(toSend);
+
 				double trig = m.RTrigger - m.LTrigger;
-				if(trig != 0){
-					String trigger = "trigger";
-					if (trig > 0){
-						trigger += "R";
-					}
-					else{
-						trigger += "L";
-					}
-					toSend += trigger + ":" + trig + "\n";
+				if( Math.abs(trig) < 0.2) trig = 0;
+
+				String trigger = "trigger";
+				if (trig > 0){
+					trigger += "R";
 				}
+				else{
+					trigger += "L";
+				}
+				toSend += trigger + ":" + trig + "\n";
+
 				socket.getOutputStream().write(toSend.getBytes());
-				
+
 			}
 			catch(Exception e ){
 				System.out.println("erreur d'envoi bluetooth: " + e.getMessage());
 			}
 		}
 	}
-	
-	
+
+
 }
